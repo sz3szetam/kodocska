0ece9453e34773c99a6fbaed5c7c02161c7b1197
Szécsi Tamás Dávid <144216869+sz3szetam@users.noreply.github.com>
2023-10-09 21:11:25 +0200

    vege
    q

--- napok.js
+++ napok.js
@@ -4,3 +4,35 @@ for(let i=0;i<napok.length;i++)
 {
     console.log(napok[i]);
 }
+console.log("for in ciklus")
+for (var index in napok) {
+    console.log(napok[index]);
+}
+console.log("for of ciklus")
+for (var nap of napok) {
+    console.log(nap);
+}
+console.log("foreach index ciklussal")
+napok.forEach(function(nap) {
+    console.log(nap);
+});
+console.log("foreach index, value ciklussal")
+napok.forEach(function(value, index) {
+    console.log("Index: " + index + ", Érték: " + value);
+});
+console.log("foreach value ciklussal")
+napok.forEach(function(value) {
+    console.log(value);
+});
+console.log("while ciklussal")
+var i = 0;
+while (i < napok.length) {
+    console.log(napok[i]);
+    i++;
+}
+console.log("do while ciklussal")
+var i = 0;
+do {
+    console.log(napok[i]);
+    i++;
+} while (i < napok.length);
